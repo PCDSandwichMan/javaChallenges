48d9a232b0ed91397a257dff1a138b3dc4d9607b
Matt <matthew.endicott@snhu.edu>
2019-03-15 17:44:19 -0600

    Initial script for the bubble sort.

---
+++ bubbleSort/bubbleSort.js
@@ -0,0 +1,27 @@
+//You are given a random array of numbers and you must loop through the array to 'sort' the contents
+
+/*
+Algorithm:
+(1) Loop through the whole array starting from index 1
+(2) If the number in the array at index i-1 is greater than i, swap the numbers and continue
+(3) Once the end of the array is reached, start looping again from the beginning
+(3) Once no more elements can be swapped, the sorting is complete
+*/
+
+const bubbleSort = (array) => {
+    let completed = true
+
+    while(completed){
+        for (i = 0; i < array.length; i++) {
+            if ()
+        }
+    }
+
+    
+};
+
+
+
+
+
+console.log(bubbleSort([3,62,1,6,32,5,6,3,2,234,53]));
